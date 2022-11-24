0e088deb05376ad4e2e5e864df34547a192aab37
“Ibra1822” <ibrahimmujagic18@gmail.com>
2022-11-24 16:18:32 +0100

    add footer structure

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -3,6 +3,42 @@ export default {
   name: "FooterComp",
 };
 </script>
-<template></template>
+<template>
+  <footer>
+    <div class="center">
+      <div class="info box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+    </div>
+  </footer>
+  <div class="find-us"></div>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "../styles/mixin" as *;
+
+footer {
+  @include debug;
+  height: 400px;
+  width: 100%;
+  .center {
+    @include center;
+    @include debug;
+    @include lowFlex;
+  }
+  .box {
+    @include debug;
+    height: 300px;
+    width: calc(100% / 4);
+  }
+  .info.box {
+    background-color: red;
+  }
+}
+.find-us {
+  height: 60px;
+  width: 100%;
+  background-color: aqua;
+}
+</style>
