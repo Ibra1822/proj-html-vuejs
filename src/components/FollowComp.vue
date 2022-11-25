a5c795af656409e99ae0dc3f04d968d661df87ef
“Ibra1822” <ibrahimmujagic18@gmail.com>
2022-11-25 11:11:09 +0100

    add two section of main

--- src/components/FollowComp.vue
+++ src/components/FollowComp.vue
@@ -6,34 +6,77 @@ export default {
 <template>
   <div class="cont-all">
     <div class="center">
-      <div class="cont-title"></div>
-      <div class="cont-form"></div>
+      <div class="cont-title">
+        <h4>NewsLetter</h4>
+        <h2><span>Know</span> First</h2>
+        <p>
+          Follow closley and receive content about our company and the news of
+          the current market
+        </p>
+      </div>
+      <div class="cont-form">
+        <input type="text" placeholder="Name" />
+        <input type="email" placeholder="Email" />
+        <button>Subscribe</button>
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 @use "../styles/mixin" as *;
-
 .cont-all {
-  @include debug;
   height: 400px;
   width: 100%;
+  background-image: url("../assets/lead-customer/images/bg-1.jpg");
+  background-size: cover;
+  background-position: right 12% bottom 56%;
   .center {
-    @include debug;
     width: 60%;
     height: 100%;
     margin: auto;
     display: flex;
     .cont-title {
-      @include debug;
       height: 100%;
       width: 50%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      padding: 20px;
+      h4 {
+        color: #1d8483;
+        text-transform: uppercase;
+        margin-bottom: 20px;
+      }
+      h2 {
+        color: #fff;
+        font-size: 2.1rem;
+        margin: 20px 0;
+        span {
+          padding: 10px 15px;
+          background-color: rgba(29, 132, 131, 0.3);
+        }
+      }
+      p {
+        color: #778a94;
+      }
     }
     .cont-form {
-      @include debug;
       height: 100%;
       width: 50%;
+      padding: 20px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      input {
+        padding: 10px 15px;
+        opacity: 0.5;
+        margin-bottom: 20px;
+      }
+      button {
+        width: 130px;
+        @include stBtn;
+      }
     }
   }
 }
--- src/components/NewsComp.vue
+++ src/components/NewsComp.vue
@@ -6,11 +6,75 @@ export default {
 <template>
   <div class="cont-news">
     <div class="center">
-      <div class="cont-title"></div>
+      <div class="cont-title">
+        <h4>Our editorial content</h4>
+        <h2>Latest <span>News</span></h2>
+        <div class="cont-p-btn">
+          <p>
+            Evry week we pubblish content about what is the best in the business
+            world
+          </p>
+          <button>See ALL</button>
+        </div>
+      </div>
       <div class="cont-box">
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
+        <div class="box">
+          <div class="img-cont">
+            <img
+              class="eff"
+              src="../assets/lead-customer/images/news-1.jpg"
+              alt=""
+            />
+          </div>
+          <div class="user">
+            <i class="fa-solid fa-user"></i>
+            <p>Anderea Miller</p>
+            <i class="fa-regular fa-clock"></i>
+            <p>2 Days ago</p>
+          </div>
+          <div class="text">
+            <h2>Increasing creativity is possible dor everyone</h2>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+        <div class="box">
+          <div class="img-cont">
+            <img
+              class="eff"
+              src="../assets/lead-customer/images/news-2.jpg"
+              alt=""
+            />
+          </div>
+          <div class="user">
+            <i class="fa-solid fa-user"></i>
+            <p>Anderea Miller</p>
+            <i class="fa-regular fa-clock"></i>
+            <p>2 Days ago</p>
+          </div>
+          <div class="text">
+            <h2>Because market research is part of of the business plan</h2>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+        <div class="box">
+          <div class="img-cont">
+            <img
+              class="eff"
+              src="../assets/lead-customer/images/news-3.jpg"
+              alt=""
+            />
+          </div>
+          <div class="user">
+            <i class="fa-solid fa-user"></i>
+            <p>Anderea Miller</p>
+            <i class="fa-regular fa-clock"></i>
+            <p>2 Days ago</p>
+          </div>
+          <div class="text">
+            <h2>Working from home is now a trend</h2>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -20,28 +84,107 @@ export default {
 @use "../styles/mixin" as *;
 
 .cont-news {
-  @include debug;
   height: 600px;
   width: 100%;
   .center {
-    @include debug;
     @include center;
     display: flex;
     flex-direction: column;
     .cont-title {
-      @include debug;
       width: 100%;
       height: 200px;
+      padding: 25px;
+      h4 {
+        color: #1d8483;
+        text-transform: uppercase;
+        margin-bottom: 20px;
+      }
+      h2 {
+        font-size: 2.1rem;
+        margin-bottom: 30px;
+        span {
+          padding: 10px 15px;
+          background-color: rgba(29, 132, 131, 0.3);
+          color: #1d8483;
+        }
+      }
+
+      .cont-p-btn {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        p {
+          color: #778a94;
+        }
+        button {
+          @include stBtn;
+        }
+      }
     }
     .cont-box {
-      @include debug;
       height: 400px;
       width: 100%;
       display: flex;
+      justify-content: space-between;
+      padding: 20px;
       .box {
-        @include debug;
         height: 350px;
-        width: calc(100% / 3);
+        width: calc(100% / 3 - 30px);
+        position: relative;
+        overflow: hidden;
+
+        &:hover .text p {
+          display: block;
+        }
+        &:hover .user {
+          display: flex;
+        }
+
+        &:hover .eff {
+          filter: blur(2px);
+        }
+
+        .img-cont {
+          height: 100%;
+          width: 100%;
+          background-color: #000;
+          border-radius: 15px;
+
+          img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            border-radius: 15px;
+            opacity: 0.5;
+          }
+        }
+
+        .user {
+          position: absolute;
+          top: 20px;
+          left: 25px;
+          display: flex;
+          color: #fff;
+          display: none;
+          p {
+            margin: 0px 15px;
+          }
+        }
+
+        .text {
+          width: 100%;
+          padding: 15px;
+          position: absolute;
+          bottom: 15px;
+          color: #fff;
+          transition: all 1s;
+
+          p {
+            display: none;
+            margin-top: 20px;
+            text-align: center;
+          }
+        }
       }
     }
   }
